99b2756be8bba55f1ea2348948f116029b81e794
Phanter2 <Ladarius.woodruff@searcyschools.org>
2023-10-24 11:15:53 -0500

    oops

--- player.gd
+++ player.gd
@@ -5,10 +5,10 @@ var is_jumping = false
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+
 
 func _physics_process(delta):
 	# Add the gravity.
